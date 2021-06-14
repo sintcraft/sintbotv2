a6c48ccc13dfe8e8c9c68188f0a18107b78ffec2
sintcraft <peladillas25@gmail.com>
2021-06-14 10:08:39 -0500

    progress the mayor's module - logic to verify message embed on channel from the config

--- src/Tools/mayorManager.js
+++ src/Tools/mayorManager.js
@@ -8,18 +8,30 @@ const setUp = async(cliente) => {
    client = cliente
    let mayorListMsg = await trackingMsg.findOne({ keyName: 'mayorList' })
    if(mayorListMsg == null){
-      let channel = client.channels.resolve(config.alcaldia.channelId)
+      let channel = client.channels.resolve(config.mayors.channelId)
       let embed = new MessageEmbed()
       embed.setColor('#efb810')
-      embed.setDescription(config.alcaldia.msgPreList+'- sint```')
+      embed.setDescription(config.mayors.msgPreList+'- ```')
       channel.send(embed).then(async(msg) => {
          await new trackingMsg({
-            channelId: config.alcaldia.channelId,
+            channelId: config.mayors.channelId,
             msgId: msg.id,
             keyName: 'mayorList',
          }).save()
       })
    }else{
+      let channel = client.channels.resolve(config.mayors.channelId)
+      let msg = await channel.messages.fetch(mayorListMsg.msgId)
+                     .catch(async() => {
+                        let embed = new MessageEmbed()
+                        embed.setColor('#efb810')
+                        embed.setDescription(config.mayors.msgPreList+'- ```')
+                        channel.send(embed).then(async(mesg) => {
+                           mayorListMsg.msgId = mesg.id
+                           await mayorListMsg.save()
+                        })
+                     })
+      console.log(channel, msg)
       // Logic to verify exists message and list all postulates of mayors in the database
    }
 }
@@ -30,6 +42,10 @@ const addMayor = async() => {
 
 const validVote = async() => {
    // Logic to verify vote valid or not valid from the schema
+}
+
+const getListMayors = async() => {
+   // Logic to list all mayors of the database and return list style
 }
 
 module.exports = {
