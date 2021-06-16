817d2070772a231c89b159620f3b98b0813a9b2f
sintcraft <peladillas25@gmail.com>
2021-06-16 08:27:07 -0500

    finished to mayor's module and mayor's commands

--- src/Tools/mayorManager.js
+++ src/Tools/mayorManager.js
@@ -21,35 +21,75 @@ const setUp = async(cliente) => {
       })
    }else{
       let channel = client.channels.resolve(config.mayors.channelId)
+      let embed = new MessageEmbed()
+      embed.setColor('#efb810')
+      embed.setDescription(config.mayors.msgPreList+await getListMayors())
       let msg = await channel.messages.fetch(mayorListMsg.msgId)
                      .catch(async() => {
-                        let embed = new MessageEmbed()
-                        embed.setColor('#efb810')
-                        embed.setDescription(config.mayors.msgPreList+'- ```')
                         channel.send(embed).then(async(mesg) => {
                            mayorListMsg.msgId = mesg.id
                            await mayorListMsg.save()
+                           return mesg
                         })
                      })
-      console.log(channel, msg)
-      // Logic to verify exists message and list all postulates of mayors in the database
+      msg.edit(embed)
    }
+   // Logic to verify exists message and list all postulates of mayors in the database
 }
 
-const addMayor = async() => {
+const updateDescription = async() => {
+   let mayorListMsg = await trackingMsg.findOne({ keyName: 'mayorList' })
+   channel = client.channels.resolve(config.mayors.channelId)
+   let embed = new MessageEmbed()
+   embed.setColor('#efb810')
+   embed.setDescription(config.mayors.msgPreList+await getListMayors())
+   let msg = await channel.messages.fetch(mayorListMsg.msgId)
+                  .catch(async() => {
+                     channel.send(embed).then(async(mesg) => {
+                        mayorListMsg.msgId = mesg.id
+                        await mayorListMsg.save()
+                        return mesg
+                     })
+                  })
+   msg.edit(embed)
    // Logic to verify exists a postulate of mayor and proposals of he's
 }
 
-const validVote = async() => {
-   // Logic to verify vote valid or not valid from the schema
+const updateVotes = async() => {
+   let mayorsData = await mayor.find()
+   for(let i=0; i < mayorsData.length; i++){
+      let user = mayorsData[i]
+      let votesData = await require('../Database/models/votes').find({ mayor: user.authorId })
+      let mayorData = await mayor.findOne({ authorId: user.authorId })
+      mayorData.votes = votesData.length
+      await mayorData.save()
+   }
+   await updateDescription()
 }
 
 const getListMayors = async() => {
-   // Logic to list all mayors of the database and return list style
+   let data = await mayor.find().sort({ timestamp: 'asc' })
+   let list = ''
+   for(let user of data) {
+      if(client.users.resolve(user.authorId)){
+         list = list + `- ${client.users.resolve(user.authorId).username} - votos ${user.votes}\n`
+      }
+   }
+   list = list + '```'
+   return list
+   // Logic to list all mayors of the database and return list style for the embed message. CHECK
+}
+
+const onMessage = async(msg) => {
+   // Logic to message event on this channel
+   setTimeout(function(){
+      msg.delete()
+   }, 3000)
 }
 
 module.exports = {
    setUp,
-   addMayor,
-   validVote,
+   updateDescription,
+   onMessage,
+   updateVotes,
 }
---
+++ src/commands/apply.js
@@ -0,0 +1,35 @@
+const mayorDB = require('../Database/models/mayor')
+const config = require('../config.json')
+const mayorManager = require('../Tools/mayorManager')
+module.exports = async(client, msg, args) => {
+   console.log('asdasdas')
+   let user = msg.author
+   let proposals = args.join(' ')
+   let data = await mayorDB.findOne({ authorId: user.id })
+   if(data == null){
+      let candidate =  new mayorDB({
+         authorId: user.id,
+         timestamp: Date.now()
+      })
+      if(args.length > 0) candidate.proposals = proposals
+      await candidate.save().then(() => {
+         msg.reply(config.mayors.successfullyApplyMsg).then((message)  => {
+            setTimeout(function(){
+               message.delete()
+            }, 5000)
+         })
+      })
+      mayorManager.updateDescription()
+   }else{
+      if(args.length > 0){
+         data.proposals = proposals
+      }
+      await data.save()
+      msg.reply(config.mayors.successfullyApplyMsg).then((message)  => {
+         setTimeout(function(){
+            message.delete()
+         }, 5000)
+      })
+      mayorManager.updateDescription()
+   }
+}
